01f276b1b9baf4c8eb1b9c223163b062fc803dcd
Vitor Pamplona <vitor@vitorpamplona.com>
2020-12-08 17:36:40 -0500

    Adding vaccination icon

--- src/assets/svgs/TabBarNav/index.ts
+++ src/assets/svgs/TabBarNav/index.ts
@@ -1,10 +1,12 @@
 import Exposure from "./exposure"
+import Vaccination from "./vaccination"
 import Heartbeat from "./heartbeat"
 import House from "./house"
 import Gear from "./gear"
 
 export const TabBarIcons = {
   Exposure,
+  Vaccination, 
   Heartbeat,
   House,
   Gear,
